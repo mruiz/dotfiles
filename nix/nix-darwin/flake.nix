4e504b29844a9a5aafd1efea978ad06b5da0bbeb
mruiz <mathieu.ruiz@gmail.com>
2025-09-18 16:54:03 +0200

    Update flake.nix and add flake.lock for improved package management

--- nix/nix-darwin/flake.nix
+++ nix/nix-darwin/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
@@ -11,8 +11,6 @@
     nix-darwin,
     nixpkgs,
     nixpkgs-unstable,
-    templ,
-    home-manager,
     ...
   } @ inputs: let
     add-unstable-packages = final: _prev: {
@@ -31,108 +29,116 @@
       # $ nix-env -qaP | grep wget
       nixpkgs.config.allowUnfree = true;
       nixpkgs.overlays = [
-        inputs.templ.overlays.default
         add-unstable-packages
       ];
       environment.systemPackages =
         [
-          # pkgs.ghostty
-          # pkgs.air # Live reload for go apps
-          # pkgs.act # Run your github Actions locally
-          # pkgs.bun # Fast nodejs
-          # pkgs.pnpm
-          # pkgs.git
-          # pkgs.gh
-          # pkgs.gnupg
-          # pkgs.unstable.go_1_25
+          pkgs.air # Live reload for go apps
+          pkgs.act # Run your github Actions locally
+          pkgs.bun # Fast nodejs
+          pkgs.pnpm
+          pkgs.git
+          pkgs.gh
+          pkgs.gnupg
+          pkgs.unstable.go_1_25
           # pkgs.lua-language-server
-          # pkgs.mkalias
+          pkgs.mkalias
           pkgs.neovim
-          # pkgs.nil
-          # #pkgs.obsidian
-          # #pkgs.opentofu
-          # pkgs.pass
-          # pkgs.rclone
-          # pkgs.ripgrep
-          # #pkgs.rustup
+          pkgs.nil
+          pkgs.obsidian
+          #pkgs.opentofu
+          pkgs.pass
+          pkgs.rclone
+          pkgs.ripgrep
+          #pkgs.rustup
           # pkgs.sqlc
           # pkgs.stylua
           # pkgs.unstable.stripe-cli
           # pkgs.tailwindcss
           # pkgs.tailwindcss-language-server
           # pkgs.qmk
-          # pkgs.templ
-          # pkgs.tmux
-          # pkgs.zoxide
+          pkgs.templ
+          pkgs.tmux
+          pkgs.zoxide
         ];
+
+      fonts.packages = [
+        pkgs.nerd-fonts.jetbrains-mono
+      ];
 
       users.users.mathieu = {
         name = username;
         home = "/Users/mathieu";
       };
 
-      # homebrew = {
-      #   enable = true;
-      #   brews = [
-      #     "mas"
-      #   ];
-      #   casks = [
-      #     "hammerspoon"
-      #     # "alfred"
-      #     "notion"
-      #     "discord"
-      #     "the-unarchiver"
-      #   ];
-      #   taps = [
-      #   ];
-      #   masApps = {
-      #     # Yoink = 457622435;
-      #   };
-      # };
+      homebrew = {
+        user = username;
+        enable = true;
+        # onActivation.cleanup = "uninstall";
+        brews = [
+          "mas"
+        ];
+        casks = [
+          "hammerspoon"
+          # "alfred"
+          "notion"
+          "discord"
+          "the-unarchiver"
+          "ghostty"
+          "android-platform-tools"
+          "monitorcontrol"
+          "autodesk-fusion"
+        ];
+        taps = [
+        ];
+        masApps = {
+          # Yoink = 457622435;
+        };
 
-      # system.activationScripts.applications.text = let
-      #   env = pkgs.buildEnv {
-      #     name = "system-applications";
-      #     paths = config.environment.systemPackages;
-      #     pathsToLink = "/Applications";
-      #   };
-      # in
-      #   pkgs.lib.mkForce ''
-      #     # Set up applications.
-      #     echo "setting up /Applications..." >&2
-      #     rm -rf /Applications/Nix\ Apps
-      #     mkdir -p /Applications/Nix\ Apps
-      #     find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-      #     while read -r src; do
-      #       app_name=$(basename "$src")
-      #       echo "copying $src" >&2
-      #       ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-      #     done
-      #   '';
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
+      # services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true; # default shell on catalina
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
-      system.stateVersion = 4;
+      system.stateVersion = 6;
 
-      # system.defaults = {
-      #   NSGlobalDomain.AppleICUForce24HourTime = true;
-      #   NSGlobalDomain.AppleShowAllExtensions = true;
-      #   loginwindow.GuestEnabled = false;
-      #   finder.FXPreferredViewStyle = "clmv";
-      # };
+      system.defaults = {
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+        NSGlobalDomain.AppleShowAllExtensions = true;
+        loginwindow.GuestEnabled = false;
+        finder.FXPreferredViewStyle = "clmv";
+      };
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
